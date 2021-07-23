e3e0a4a0d1250f3948e01956560a54cc9c459d13
NiratKashom <earthenly@gmail.com>
2021-07-23 10:18:41 +0700

    setup css, index, login/register form, some schedule page

---
+++ CC9_personalProject/read_me.txt
@@ -0,0 +1,5 @@
+7/23/2021
+-setup css
+-homepage
+-destinaiton page
+-login/register form
---
+++ CC9_personalProject/space_travel/script.js
@@ -0,0 +1,11 @@
+// const allBtn = document.querySelectorAll('button');
+
+// console.log(allBtn);
+
+// allBtn.forEach(item => {
+//   item.addEventListener('click',event => {
+//       event.preventDefault();
+//      }   
+//   );
+// });
+
